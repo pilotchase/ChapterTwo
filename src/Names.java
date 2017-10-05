e63c44f260243afe031f65fcd57455d97e4f0bec
Chase Morgan <pilotchase@users.noreply.github.com>
2017-10-05 11:59:04 -0700

    Initial Commit

---
+++ src/Names.java
@@ -0,0 +1,11 @@
+public class Names {
+    public static void main (String[] args)
+    {
+        System.out.println ();
+        System.out.println ("\tName\t\tHometown");
+        System.out.println ("\t====\t\t========");
+        System.out.println ("\tSally\t\tRoanoke");
+        System.out.println ("\tAlexander\tWashington");
+        System.out.println ();
+    }
+}
---
+++ src/Points.java
@@ -0,0 +1,13 @@
+public class Points {
+    public static void main(String[] args)
+    {
+        System.out.println("\t-------------------------------");
+        System.out.println("\t==\tStudent Points\t==");
+        System.out.println("\t-------------------------------");
+        System.out.println("\tName\tLab\tBonus\tTotal");
+        System.out.println("\t-----\t----\t-----\t----");
+        System.out.println("\tJoe\t43\t7\t50");
+        System.out.println("\tWilliam\t50\t8\t58");
+        System.out.println("\tMarySue 39\t10\t49");
+    }
+}
